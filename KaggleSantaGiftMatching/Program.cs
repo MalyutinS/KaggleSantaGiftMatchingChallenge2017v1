56a7b5c63a267bb16a0b05a02495ca239e24b641
sergey <malyutins@gmail.com>
2017-12-23 19:50:18 +0300

    refactoring and optimization

--- KaggleSantaGiftMatching/Program.cs
+++ KaggleSantaGiftMatching/Program.cs
@@ -10,13 +10,12 @@ namespace KaggleSantaGiftMatching
     {
 
 
-        public static double MIN_FLOW = -101 / 2000000.0; // 2.0505
+        public static double MinFlow = -101 / 2000000.0; // 
 
-        static void Main(string[] args)
+        static void Main()
         {
 
             var cm = new int[1000000, 10];
-
             using (var reader = new StreamReader("child_wishlist.csv"))
             {
                 var childId = 0;
@@ -32,11 +31,11 @@ namespace KaggleSantaGiftMatching
                     childId++;
                 }
             }
-
             Console.WriteLine("{0}: child_wishlist.csv - OK", DateTime.Now);
 
-            var gm = new int[1000, 1000];
 
+
+            var gm = new int[1000, 1000];
             using (var reader = new StreamReader("gift_goodkids.csv"))
             {
                 var giftId = 0;
@@ -52,19 +51,19 @@ namespace KaggleSantaGiftMatching
                     giftId++;
                 }
             }
-
             Console.WriteLine("{0}: gift_goodkids.csv - OK", DateTime.Now);
 
-            var edgeMap = new Dictionary<Tuple<int, int>, double>();
 
+
+            var edgeMap = new Dictionary<int, double>();
 
             for (int i = 0; i < 1000000; i++)
             {
                 for (int k = 0; k < 10; k++)
                 {
                     var j = cm[i, k];
-                    var edge1 = new Tuple<int, int>(j, i);
-                    edgeMap[edge1] = (200 * (10 - k) - 1) / 2000000.0; /// !!!!!!!!!!!
+                    var edge1 = i*1000 + j;
+                    edgeMap[edge1] = (200 * (10 - k) - 1) / 2000000.0; // !!!!!!!!!!!
                 }
             }
 
@@ -73,7 +72,7 @@ namespace KaggleSantaGiftMatching
                 for (int k = 0; k < 1000; k++)
                 {
                     var i = gm[j, k];
-                    var edge1 = new Tuple<int, int>(j, i);
+                    var edge1 = i * 1000 + j;
                     if (edgeMap.ContainsKey(edge1))
                     {
                         edgeMap[edge1] += (2 * (1000 - k) + 1) / 2000000.0; // !!!!!!!!!!!!!!!!
@@ -89,48 +88,58 @@ namespace KaggleSantaGiftMatching
             {
                 for (int j = 0; j < 1000; j++)
                 {
-                    var edge = new Tuple<int, int>(j, i);
+                    var edge = i * 1000 + j;
                     if (!edgeMap.ContainsKey(edge))
                     {
-                        edgeMap[edge] = MIN_FLOW; /// !!!!!!!!!!!!!!
+                        edgeMap[edge] = MinFlow; // !!!!!!!!!!!!!!
                     }
                 }
             }
 
+            var totalArcs = edgeMap.Count;
 
+            Console.WriteLine("{1}: edgeMap - ok ({0})", totalArcs, DateTime.Now);
 
+            // to define objective
+            var costs = new double[totalArcs];
+            // to define constraints
+            var edge2Ind = new Dictionary<int, int>(totalArcs);
+            var i2J = new List<List<short>>(10000000);
+            var j2I = new List<List<int>>(1000);
+            // to restore solution from CPLEX output
+            var ind2Edge = new int[totalArcs];
 
-            Console.WriteLine("{1}: edgeMap - OK ({0})", edgeMap.Count, DateTime.Now);
-
-            // new dict helper
-            var edge2ind = new Dictionary<Tuple<int, int>, int>();
-            var ind2edge = new Dictionary<int, Tuple<int, int>>();
-            var i2j = new Dictionary<int, List<int>>();
-            var j2i = new Dictionary<int, List<int>>();
+            for (int i=0; i < 1000000; i++)
+            {
+                i2J.Add(new List<short>());
+            }
+            for (int j = 0; j < 1000; j++)
+            {
+                j2I.Add(new List<int>());
+            }
 
             int t = 0;
             foreach (var edge in edgeMap)
             {
-                edge2ind[edge.Key] = t;
-                ind2edge[t] = edge.Key;
-                if (!i2j.ContainsKey(edge.Key.Item2))
-                {
-                    i2j[edge.Key.Item2] = new List<int>();
-                }
-                i2j[edge.Key.Item2].Add(edge.Key.Item1);
-                if (!j2i.ContainsKey(edge.Key.Item1))
-                {
-                    j2i[edge.Key.Item1] = new List<int>();
-                }
-                j2i[edge.Key.Item1].Add(edge.Key.Item2);
+                int i = edge.Key / 1000;
+                short j = (short)(edge.Key % 1000);
+
+                costs[t] = edge.Value;
+                edge2Ind[edge.Key] = t;
+                i2J[i].Add(j);
+                j2I[j].Add(i);
+                ind2Edge[t] = edge.Key;
+
                 t++;
             }
+            edgeMap = null;
 
-            Console.WriteLine("{1}: edge2ind - ok ({0})", edge2ind.Count, DateTime.Now);
-            Console.WriteLine("i2jCost - ok ({0})", i2j.Count);
-            Console.WriteLine("j2iCost - ok ({0})", j2i.Count);
-
-            // total arcs edgeMap.Count + 1000000 + 1000 + 1
+            Console.WriteLine("{1}: cost - ok ({0})", costs.Length, DateTime.Now);
+            Console.WriteLine("{1}: edge2ind - ok ({0})", edge2Ind.Count, DateTime.Now);
+            Console.WriteLine("{1}: i2j - ok ({0})", i2J.Count, DateTime.Now);
+            Console.WriteLine("{1}: j2i - ok ({0})", j2I.Count, DateTime.Now);
+            Console.WriteLine("{1}: ind2edge - ok ({0})", ind2Edge.Length, DateTime.Now);
+            
 
             Console.WriteLine("{0}: start cplex", DateTime.Now);
 
@@ -138,39 +147,30 @@ namespace KaggleSantaGiftMatching
             try
             {
                 cplex = new Cplex();
-                cplex.SetParam(Cplex.DoubleParam.TiLim, 2 * 60 * 60); // in seconds
+                // 2h time limit in seconds
+                cplex.SetParam(Cplex.DoubleParam.TiLim, 2 * 60 * 60); 
+                // mip tolerances (absolute and relative) that I set to 0. 
+                // If I don't set them, then I get a solution 0.00000071 worse
+                cplex.SetParam(Cplex.Param.MIP.Tolerances.MIPGap, 0); 
+                cplex.SetParam(Cplex.Param.MIP.Tolerances.AbsMIPGap, 0); 
 
-                // My solution is obtained at root node of CPLEX mip with default settings except for mip tolerances (absolute and relative) that I set to 0. If I don't set them, then I get a solution 1e-8 worse. !!!!!!!!!!!!!!
-                cplex.SetParam(Cplex.Param.MIP.Tolerances.MIPGap, 0); // !!
-                cplex.SetParam(Cplex.Param.MIP.Tolerances.AbsMIPGap, 0); // !!
+                INumVar[][] var = new INumVar[1][];
+                IRange[][] rng = new IRange[3][];
 
-                INumVar[][] var = new INumVar[3][];
-                IRange[][] rng = new IRange[5][];
+                var[0] = cplex.BoolVarArray(totalArcs + 1000000 - 4000);
+                //var[1] = cplex.IntVarArray(1000, 0, 1000);
+                //var[2] = cplex.IntVarArray(1, 0, 1000000 - 4000);
 
-                var[0] = cplex.BoolVarArray(edgeMap.Count + 1000000 - 4000);
-                var[1] = cplex.IntVarArray(1000, 0, 1000);
-                var[2] = cplex.IntVarArray(1, 0, 1000000 - 4000);
-
-                DescribeModel(cplex, edgeMap, var, rng, edge2ind, i2j, j2i);
-
+                DescribeModel(cplex, costs, var, rng, edge2Ind, i2J, j2I);
+                
 
                 Console.WriteLine("{0}: start solve", DateTime.Now);
 
-                var child2gift = new Dictionary<int, int>();
-                var giftCount = new Dictionary<int, int>();
-                for (int j = 0; j < 1000; j++)
-                {
-                    giftCount[j] = 1000;
-                }
-
-
                 if (cplex.Solve())
                 {
-                    Console.WriteLine("{0}: Solved !!!!!!!!!!!!!!!!!!!!!!!!!!!!", DateTime.Now);
+                    Console.WriteLine("{0}: Solved !", DateTime.Now);
                     Console.WriteLine("Solution status = " + cplex.GetStatus());
                     Console.WriteLine("Solution value  = " + cplex.ObjValue);
-
-
                 }
                 else
                 {
@@ -180,65 +180,51 @@ namespace KaggleSantaGiftMatching
 
                 double[] outX = cplex.GetValues(var[0]);
 
+                var child2Gift = new short[1000000];
+                var giftCount = new int[1000];
+                for (int i = 0; i < 1000000; i++)
+                {
+                    giftCount[i] = -1;
+                }
+                for (int j = 0; j < 1000; j++)
+                {
+                    giftCount[j] = 1000;
+                }
 
                 var total = 0;
-                for (int k = 0; k < edgeMap.Count; k++)
+                for (int k = 0; k < totalArcs; k++)
                 {
                     if (outX[k] > 0.9 && outX[k] < 1.1)
                     {
-                        var edge = ind2edge[k];
-                        int i = edge.Item2;
-                        int j = edge.Item1;
-                        child2gift[i] = j;
+                        var edge = ind2Edge[k];
+                        int i = edge / 1000;
+                        short j = (short)(edge % 1000);
+                        child2Gift[i] = j;
                         giftCount[j]--;
                         total++;
                     }
                 }
-                Console.WriteLine("Total: {0}", total);
-                for (int i = 0; i < 1000000 - 4000; i++)
-                //for (int i = 0; i < 1000000 ; i++)
-                {
-                    if (outX[edgeMap.Count + i] > 0.9 && outX[edgeMap.Count + i] < 1.1)
-                    {
-                        total++;
-                    }
-                }
-                Console.WriteLine("Total: {0}", total);
+                Console.WriteLine("{1}: Total number of childs with gifts after MIP: {0}", total, DateTime.Now);
 
 
                 for (int i = 0; i < 1000000; i++)
                 {
-                    if (!child2gift.ContainsKey(i))
+                    if (child2Gift[i] == -1)
                     {
                         Console.WriteLine("no gift for {0}", i);
                         if (i < 4000)
                         {
                             Console.WriteLine("shouldn't happen {0}", i);
-                            // twins
-                            for (int j = 0; j < 1000; j++)
-                            {
-                                if (giftCount[j] > 1)
-                                {
-                                    child2gift[i] = j;
-                                    child2gift[i + 1] = j;
-                                    giftCount[j] -= 2;
-                                    break;
-                                }
-                            }
-                            if (!child2gift.ContainsKey(i))
-                            {
-                                Console.WriteLine("no gift for twin {0}", i);
-                            }
                         }
                         else
                         {
                             //find first available gift
-                            for (int j = 0; j < 1000; j++)
+                            for (short j = 0; j < 1000; j++)
                             {
                                 if (giftCount[j] > 0)
                                 {
                                     Console.WriteLine("gift {0} assigned to {1}", j, i);
-                                    child2gift[i] = j;
+                                    child2Gift[i] = j;
                                     giftCount[j]--;
                                     break;
                                 }
@@ -247,19 +233,10 @@ namespace KaggleSantaGiftMatching
                     }
                 }
 
+                
 
-                Console.WriteLine("{0}: All gift assigned", DateTime.Now);
-
-                for (int j = 0; j < 1000; j++)
-                {
-                    if (giftCount[j] > 0)
-                    {
-                        Console.WriteLine("{0}: {1}", j, giftCount[j]);
-                    }
-                }
-
-                Console.WriteLine("{0}: dict to csv", DateTime.Now);
-                // write to csv
+                Console.WriteLine("{0}: write solution to csv", DateTime.Now);
+                
 
                 using (var w = new StreamWriter("my.csv"))
                 {
@@ -267,15 +244,15 @@ namespace KaggleSantaGiftMatching
                     w.Flush();
                     for (int i = 0; i < 1000000; i++)
                     {
-                        if (child2gift.ContainsKey(i))
+                        if (child2Gift[i] >= 0)
                         {
-                            var line = string.Format("{0},{1}", i, child2gift[i]);
+                            var line = string.Format("{0},{1}", i, child2Gift[i]);
                             w.WriteLine(line);
                             w.Flush();
                         }
                         else
                         {
-                            Console.WriteLine("child {0} has no gift", i);
+                            Console.WriteLine("Error: child {0} has no gift", i);
                         }
 
                     }
@@ -294,55 +271,38 @@ namespace KaggleSantaGiftMatching
                     cplex.End();
                 }
             }
-
-
         }
 
 
+        
 
-        // public static double Cost() { }
-
-        /// <summary>
-        /// 
-        /// </summary>
-        /// <param name="cplex"></param>
-        /// <param name="edgeMap"></param>
-        /// <param name="var"></param>
-        /// <param name="rng"></param>
-        /// <param name="edge2ind"></param>
-        /// <param name="i2j"></param>
-        /// <param name="j2i"></param>
+        
         public static void DescribeModel(Cplex cplex,
-            Dictionary<Tuple<int, int>, double> edgeMap,
+            double[] costs,
             INumVar[][] var,
             IRange[][] rng,
-            Dictionary<Tuple<int, int>, int> edge2ind,
-            Dictionary<int, List<int>> i2j,
-            Dictionary<int, List<int>> j2i)
+            Dictionary<int, int> edge2Ind,
+            List< List<short>> i2J,
+            List< List<int>> j2I)
         {
             IIntVar[] x = (IIntVar[])var[0];
-            IIntVar[] y = (IIntVar[])var[1];
-            IIntVar[] z = (IIntVar[])var[2];
 
+            int totalArcs = costs.Length;
 
             // objective
-            INumExpr[] prodsObj = new INumExpr[edgeMap.Count + 1000000 - 4000];
-            //INumExpr[] prodsObj = new INumExpr[edgeMap.Count + 1000000];
-            int k = 0;
-            foreach (var edge in edgeMap)
+            INumExpr[] prodsObj = new INumExpr[costs.Length + 1000000 - 4000];
+            for (int i = 0; i < totalArcs; i++)
             {
-                prodsObj[k] = cplex.Prod(x[k], edge.Value);
-                k++;
+                prodsObj[i] = cplex.Prod(x[i], costs[i]);
             }
-            for (int i = 0; i < 1000000 - 4000; i++)
-            //for (int i = 0; i < 1000000; i++)
+            for (int i = totalArcs; i < totalArcs + 1000000 - 4000; i++)
             {
-                prodsObj[k] = cplex.Prod(x[k], MIN_FLOW); // !!!!!!!!!
-                k++;
+                prodsObj[i] = cplex.Prod(x[i], MinFlow); 
             }
             cplex.AddMaximize(cplex.Sum(prodsObj));
             Console.WriteLine("{0}: cplex Objective - ok", DateTime.Now);
-
+            costs = null;
+            
 
 
             // flow to childs
@@ -350,19 +310,20 @@ namespace KaggleSantaGiftMatching
             for (int i = 0; i < 1000000; i++)
             {
                 var prods = new List<INumExpr>();
-                foreach (var j in i2j[i])
+                foreach (var j in i2J[i])
                 {
-                    prods.Add(x[edge2ind[new Tuple<int, int>(j, i)]]);
+                    prods.Add(x[edge2Ind[i*1000+j]]);
                 }
                 if (i >= 4000)
                 {
-                    prods.Add(x[edgeMap.Count + i - 4000]);
+                    prods.Add(x[totalArcs + i - 4000]);
                 }
                 //prods.Add(x[edgeMap.Count + i]);
 
                 rng[0][i] = cplex.AddEq(cplex.Sum(prods.ToArray()), 1);
             }
-            Console.WriteLine("{0}: cond1 - OK", DateTime.Now);
+            i2J = null;
+            Console.WriteLine("{0}: constraint to ensure each child has a gift - OK", DateTime.Now);
 
 
 
@@ -371,67 +332,39 @@ namespace KaggleSantaGiftMatching
             for (int j = 0; j < 1000; j++)
             {
                 var prods = new List<INumExpr>();
-                foreach (var i in j2i[j])
+                foreach (var i in j2I[j])
                 {
-                    prods.Add(x[edge2ind[new Tuple<int, int>(j, i)]]);
+                    prods.Add(x[edge2Ind[i * 1000 + j]]);
                 }
-                prods.Add(cplex.Prod(y[j], -1));
-                rng[1][j] = cplex.AddEq(cplex.Sum(prods.ToArray()), 0);
-                //rng[1][j] = cplex.AddLe(cplex.Sum(prods.ToArray()), 1000);
+                //prods.Add(cplex.Prod(y[j], -1));
+                //rng[1][j] = cplex.AddEq(cplex.Sum(prods.ToArray()), 0);
+                rng[1][j] = cplex.AddLe(cplex.Sum(prods.ToArray()), 1000);
             }
-            Console.WriteLine("{0}: cond2 - OK", DateTime.Now);
-
-            //
-            rng[2] = new IRange[1];
-            var prods2 = new List<INumExpr>();
-            //for (int i = 0; i < 1000000 ; i++)
-            for (int i = 0; i < 1000000 - 4000; i++)
-            {
-                prods2.Add(x[edgeMap.Count + i]);
-            }
-            prods2.Add(cplex.Prod(z[0], -1));
-            rng[2][0] = cplex.AddEq(cplex.Sum(prods2.ToArray()), 0);
-
-            // 
-            rng[3] = new IRange[1];
-            var prods3 = new List<INumExpr>();
-            for (int j = 0; j < 1000; j++)
-            {
-                prods3.Add(y[j]);
-            }
-            prods3.Add(z[0]);
-            rng[3][0] = cplex.AddEq(cplex.Sum(prods3.ToArray()), 1000000);
+            j2I = null;
+            Console.WriteLine("{0}: constraint to ensure we do not use more than 1000 of each gift - OK", DateTime.Now);
+            
 
             // twins
-
-            var tmp = new List<IRange>();
+            rng[2] = new IRange[2000*1000];
             int r = 0;
-            k = 0;
-            foreach (var edge1 in edgeMap)
+            foreach(var edge1 in edge2Ind)
             {
-                int i = edge1.Key.Item2;
-                int j = edge1.Key.Item1;
+                int i = edge1.Key / 1000;
                 if (i < 4000 && i % 2 == 0)
                 {
-                    var edge2 = new Tuple<int, int>(j, i + 1);
-
-                    var m = edge2ind[edge2];
-                    //Console.WriteLine("{0}={1}", k, m);
-                    tmp.Add(cplex.AddEq(cplex.Sum(x[k], cplex.Prod(x[m], -1)), 0));
-                    //rng[4][r] = cplex.AddEq(cplex.Sum(x[k], cplex.Prod(x[m], -1)), 0);
-
+                    short j = (short)(edge1.Key % 1000);
+                    var m = edge2Ind[(i + 1) * 1000 + j];
+                    rng[2][r] = cplex.AddEq(cplex.Sum(x[edge1.Value], cplex.Prod(x[m], -1)), 0);
                     r++;
                 }
-                k++;
             }
-
-            rng[4] = tmp.ToArray();
-            Console.WriteLine("{1}: twins r = {0}", r, DateTime.Now);
+            edge2Ind = null;
+            Console.WriteLine("{0}: twins constraint", DateTime.Now);
 
 
 
             // export
-            cplex.ExportModel("milp-workers.lp");
+            //cplex.ExportModel("milp-workers.lp");
         }
     }
 }
